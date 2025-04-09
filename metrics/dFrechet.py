ccc5400c356df45d0bd7c9979bd2c21bc9344f53
Soline270dC <93125031+Soline270dC@users.noreply.github.com>
2025-04-09 14:37:40 +0200

    reduced space complexity of Fréchet distance and Dynamic Time Warping

--- metrics/dFrechet.py
+++ metrics/dFrechet.py
@@ -8,17 +8,19 @@ def frechet_distance(P, Q, d = lambda x, y : np.linalg.norm(x - y)) :
     d : callable, distance to be used on data contained by P and Q
     """
     p, q = len(P), len(Q)
-    c = np.zeros((p,q))
+    c = np.zeros((2, q))
     c[0, 0] = d(P[0], Q[0])
-
-    for i in range(1, p) :
-        c[i, 0] = max(c[i-1, 0], d(P[i], Q[0]))
+    c[1, 0] = max(c[0, 0], d(P[1], Q[0]))
     
     for j in range(1, q) :
         c[0, j] = max(c[0, j-1], d(P[0], Q[j]))
+        c[1, j] = max(d(P[1], Q[j]), min(c[0, j], c[1, j-1], c[0, j-1]))
     
     for i in range(1, p) :
+        c[0, 0] = c[1, 0]
+        c[1, 0] = max(c[0, 0], d(P[i], Q[0]))
         for j in range(1, q) :
-            c[i, j] = max(d(P[i], Q[j]), min(c[i-1, j], c[i, j-1], c[i-1, j-1]))
+            c[0, j] = c[1, j]
+            c[1, j] = max(d(P[i], Q[j]), min(c[0, j], c[1, j-1], c[0, j-1]))
 
     return c[-1,-1]
